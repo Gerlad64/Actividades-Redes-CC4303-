787ba199529212b9de9a5d8bc34e0385583c71be
Gerlad64 <gerald.ponce@ug.uchile.cl>
2026-08-15 21:35:16 -0400

    feat: añade config.py para gestionar configuraciones del programa

---
+++ Actividad-1/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+BASE_DIR: Path = Path(__file__).parent
+HTML_PATH: Path = BASE_DIR / 'index.html'
+
+SERVER_NAME: str = "http_server.py/Actividad-1"
+SERVER_CONNECTION: str = "close"
+SERVER_ADDRESS: tuple[str, int] = ('10.192.1.3', 8080)
+SERVER_BUFFER_SIZE: int = 4
+SERVER_WORKERS: int = 4
+
+
+
