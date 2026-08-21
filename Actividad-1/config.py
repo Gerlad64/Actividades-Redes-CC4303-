5ec81a269d2a77ceec6ea5fab4bbc4487703a988
Gerlad64 <gerald.ponce@ug.uchile.cl>
2026-08-21 00:52:50 -0400

    refactor: se cambia HTML_PATH para servir index-gta.html en lugar de index.html

--- Actividad-1/config.py
+++ Actividad-1/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 BASE_DIR: Path = Path(__file__).parent
-HTML_PATH: Path = BASE_DIR / 'index.html'
+HTML_PATH: Path = BASE_DIR / 'index-gta.html'
 PROXY_JSON_PATH: Path = BASE_DIR / 'proxy.json'
 
 SERVER_NAME: str = "http_server.py/Actividad-1"
