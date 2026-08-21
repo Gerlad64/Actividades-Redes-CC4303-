3dbac8053b2edf936c8b08f3f30f2322d88c9a94
Gerlad64 <gerald.ponce@ug.uchile.cl>
2026-08-20 23:50:11 -0400

    feat: se añade configuración PROXY_JSON_PATH
    Se especifica en la tarea que se debe poder leer desde un json
    La ruta al json debe ser especificada desde las configuraciones

--- Actividad-1/config.py
+++ Actividad-1/config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 BASE_DIR: Path = Path(__file__).parent
 HTML_PATH: Path = BASE_DIR / 'index.html'
+PROXY_JSON_PATH: Path = BASE_DIR / 'proxy.json'
 
 SERVER_NAME: str = "http_server.py/Actividad-1"
 SERVER_CONNECTION: str = "close"
