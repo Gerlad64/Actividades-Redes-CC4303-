bb27d9244b050baf448cc939db3c6ad2c05daa94
Gerlad64 <gerald.ponce@ug.uchile.cl>
2026-08-23 23:31:48 -0400

    refactor/feat: modifica la lógica del servidor proxy cumplir con los puntos 2 a 3 de la parte 2

--- Actividad-1/http_server.py
+++ Actividad-1/http_server.py
@@ -36,11 +36,6 @@ headers = {
 def http_proxy_request(start_line: str) -> HTTP:
     return HTTP(start_line, headers, "")
 
-HTTP_RESPONSE: HTTP = HTTP.from_html(html)
-HTTP_RESPONSE.headers["Server"] = SERVER_NAME
-HTTP_RESPONSE.headers["Connection"] = SERVER_CONNECTION
-HTTP_RESPONSE.headers["X-ElQuePregunta"] = proxy_config.user
-
 
 if __name__ == '__main__':
 
@@ -56,34 +51,50 @@ if __name__ == '__main__':
 
         # Lógica Proxy
         if http_req.headers.get("Host", f"{SERVER_ADDRESS[0]}:{SERVER_ADDRESS[1]}") != f"{SERVER_ADDRESS[0]}:{SERVER_ADDRESS[1]}":
-            # Crear socket para conexión con servidor de destino
-            proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-            # Obtiene la dirección a partir del header
-            # la clase HTTP asegura que está bien formateado
-            # la dirección puede ser un dominio sin puerto, ya que por defecto es el 80
-            address = http_req.headers["Host"].split(":")
-            ip = address[0]
+            # Primero verifica que el sitio no esté prohibido
+            if proxy_config.is_forbidden(http_req.start_line):
+                http_res = HTTP.from_html(STATUS_403, 403, "Forbidden")
+            else:
+                # Crear socket para conexión con servidor de destino
+                proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-            port = 80 if len(address) == 1 else int(address[1])
+                # Obtiene la dirección a partir del header
+                # la clase HTTP asegura que está bien formateado
+                # la dirección puede ser un dominio sin puerto, ya que por defecto es el 80
+                address = http_req.headers["Host"].split(":")
+                ip = address[0]
 
-            print("Proxy Request:", http_proxy_request(http_req.start_line).create_message(), sep='\n')
-            # Se manda el mensaje http desde el proxy y se recibe una respuesta
-            http_res: HTTP = http_proxy_request(http_req.start_line).send_to(
-                conn_socket=proxy_socket,
-                address=(ip, port),
-                buffer_size=SERVER_BUFFER_SIZE
-            )
-            # Se termina la conexión
-            proxy_socket.close()
+                port = 80 if len(address) == 1 else int(address[1])
+
+                print("Proxy Request:", http_proxy_request(http_req.start_line).create_message(), sep='\n')
+                # Se manda el mensaje http desde el proxy y se recibe una respuesta
+                http_res: HTTP = http_proxy_request(http_req.start_line).send_to(
+                    conn_socket=proxy_socket,
+                    address=(ip, port),
+                    buffer_size=SERVER_BUFFER_SIZE
+                )
+                # Se termina la conexión
+                proxy_socket.close()
+                http_res = proxy_config.apply(http_res)
             print("Sending Response:", http_res.create_message(), sep='\n')
             # Se manda el http recibido desde el proxy
             new_socket.send(http_res.create_message().encode())
 
         else: # Si no se realiza una petición a un sitio externo, se retorna
-            response_msg = HTTP_RESPONSE.create_message()
-            print("Sending Response:", response_msg.split('\r\n\r\n', 1)[0], sep='\n')
-            new_socket.send(response_msg.encode())
-            print("Closing connection")
+            route = http_req.start_line.split()[1][len(f"http://{SERVER_ADDRESS[0]}:{SERVER_ADDRESS[1]}/"):]
+            print("route: ",route)
+            if len(route) <= 1:
+                response_bytes = HTTP.from_html(
+                    html,
+                ).create_message_bytes()
+                print("Sending Response:", response_bytes.decode().split('\r\n\r\n', 1)[0], sep='\n')
+            else:
+                response_bytes = HTTP.from_file(
+                    BASE_DIR / Path(route),
+                    STATUS_404
+                ).create_message_bytes()
+            new_socket.send(response_bytes)
 
+        print("Closing connection")
         new_socket.close()
