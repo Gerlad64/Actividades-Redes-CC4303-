d7c915f2b3f83a44732ed93391bb73436ade05fd
Gerlad64 <gerald.ponce@ug.uchile.cl>
2026-08-23 23:32:33 -0400

    refactor: cambia de lugar index-gta.html para estar en html/

--- Actividad-1/config.py
+++ Actividad-1/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 BASE_DIR: Path = Path(__file__).parent
-HTML_PATH: Path = BASE_DIR / 'index-gta.html'
+HTML_PATH: Path = BASE_DIR / "html" / 'index-gta.html'
 PROXY_JSON_PATH: Path = BASE_DIR / 'proxy.json'
 STATUS_404_PATH: Path = BASE_DIR / 'html' / 'status' /'404.html'
 STATUS_403_PATH: Path = BASE_DIR / 'html' / 'status' /'403.html'
