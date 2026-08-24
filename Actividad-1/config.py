cdc077948346396cb961ad1b5b5e30c0dc8df70f
Gerlad64 <gerald.ponce@ug.uchile.cl>
2026-08-23 23:08:14 -0400

    feat: añade imagenes y páginas html para status de error HTTP
    Introduce directorio html, similar a nginx, donde en esta estructura de directorios se encontraran los recursos del servidor
    Dentro de html/ se encuentran images para imagene y status/ para paginas html con status de error http

--- Actividad-1/config.py
+++ Actividad-1/config.py
@@ -3,6 +3,8 @@ from pathlib import Path
 BASE_DIR: Path = Path(__file__).parent
 HTML_PATH: Path = BASE_DIR / 'index-gta.html'
 PROXY_JSON_PATH: Path = BASE_DIR / 'proxy.json'
+STATUS_404_PATH: Path = BASE_DIR / 'html' / 'status' /'404.html'
+STATUS_403_PATH: Path = BASE_DIR / 'html' / 'status' /'403.html'
 
 SERVER_NAME: str = "http_server.py/Actividad-1"
 SERVER_CONNECTION: str = "close"
